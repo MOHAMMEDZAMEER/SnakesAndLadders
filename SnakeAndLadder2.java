31c8f6991ff1d85e423e84587353d20a09398a09
MOHAMMEDZAMEER <zameer4939@gmail.com>
2021-11-05 08:53:35 -0700

    UC-2 added

---
+++ SnakeAndLadder2.java
@@ -0,0 +1,14 @@
+public class SnakeAndLadder2{
+
+	public static void main(String[] args) {
+		
+		int position = 0;
+		System.out.println(" Welcome player!! ");
+		
+		System.out.println(" Current position: " + position);
+		
+		double dice = Math.floor( (Math.random() * 10) % 6 + 1); 
+		
+	}
+
+}
