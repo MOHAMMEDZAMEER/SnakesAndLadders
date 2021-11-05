d472ead9519910172866faff523c2feb3992c42a
MOHAMMEDZAMEER <zameer4939@gmail.com>
2021-11-05 08:44:22 -0700

    position set to 0

---
+++ SnakeAndLadder1.java
@@ -0,0 +1,12 @@
+public class SnakeAndLadder1 {
+
+	public static void main(String[] args) {
+		
+		int position = 0;
+		System.out.println(" Welcome player!! ");
+		
+		System.out.println(" Current position: " + position); 
+		
+	}
+
+}
